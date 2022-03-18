4a3045478731a54cb56ea33ddc727db105a4999f
Christian Mendoza <patts2810@gmail.com>
2022-03-18 15:32:09 -0500

    feat: add tests to login

---
+++ src/test/components/login/LoginScreen.test.js
@@ -0,0 +1,48 @@
+import { MemoryRouter, Routes, Route } from "react-router-dom";
+import { mount } from "enzyme";
+import { AuthContext } from "../../../auth/authContext";
+import { LoginScreen } from "../../../components/login/LoginScreen";
+
+const mockNavigate = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockNavigate,
+}));
+
+describe("Tests on <LoginScreen />", () => {
+  const contextValue = {
+    dispatch: jest.fn(),
+    user: {
+      name: "Jona",
+      logged: true,
+    },
+  };
+
+  const wrapper = mount(
+    <AuthContext.Provider value={contextValue}>
+      <MemoryRouter initialEntries={["/login"]}>
+        <Routes>
+          <Route path="/login" element={<LoginScreen />} />
+        </Routes>
+      </MemoryRouter>
+    </AuthContext.Provider>
+  );
+
+  test("should match with snapshot", () => {
+
+    const handleClick = wrapper.find(".login").prop("onClick");
+    handleClick();
+
+    expect(wrapper).toMatchSnapshot();
+    expect(contextValue.dispatch).toHaveBeenCalledWith({
+      payload: { name: "Patts" },
+      type: "[auth] Login",
+    });
+    expect(mockNavigate).toHaveBeenCalledWith("/marvel", { replace: true });
+
+    localStorage.setItem("lastPath", "/dc");
+    handleClick();
+    expect(mockNavigate).toHaveBeenCalledWith("/dc", { replace: true });
+  });
+});
